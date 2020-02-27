5d1abd343e2b173b1fa3b7f5a0def1903c05f613
Tay <taylor.mleigh@gmail.com>
2020-02-27 13:37:55 -0600

    adding author quotes

--- client/src/components/Quote.jsx
+++ client/src/components/Quote.jsx
@@ -8,7 +8,8 @@ export default function Quote() {
     `"A professional writer is an amateur who didn’t quit.”\n― Richard Bach`,
     `"If there's a book that you want to read, but it hasn't been written yet, then you must write it."\n― Toni Morrison`,
     `"I write to discover what I know."\n― Flannery O'Connor`,
-
+    `"Your intuition knows what to write, so get out of the way."\n― Ray Bradbury.`,
+    `"Writing is an exploration. You start from nothing and learn as you go."\n― E. L. Doctorow.`,
   ]
 
   let random = Math.ceil(Math.random() * quotes.length-1);
