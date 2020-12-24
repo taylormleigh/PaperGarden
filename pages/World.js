61f83ac095f8164ece922be4f1e5b91941ee6aed
Tay <taylor.mleigh@gmail.com>
2020-12-24 16:41:57 -0600

    planning inputs

--- pages/World.js
+++ pages/World.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Layout from '../components/layout';
 
-export default function World({index, world, worldView, click, worldName}) {
+export default function World({worldIndex}) {
   // console.log("worldView.jsx: ", world.formfill);
   // var parsed = JSON.parse(world.formfill);
   // var keys = Object.keys(parsed);
@@ -13,14 +13,14 @@ export default function World({index, world, worldView, click, worldName}) {
       all the world info!
       <p/>
       <br/>world
-      <br/>->region
-      <br/>->->City
-      <br/>->->city
-      <br/>->region
-      <br/>->->city
-      <br/>->region
-      <br/>->region
-      <br/>->->city
+      <br/>->world.region
+      <br/>->->region.city
+      <br/>->->region.city
+      <br/>->world.region
+      <br/>->->region.city
+      <br/>->world.region
+      <br/>->world.region
+      <br/>->->region.city
     </Layout>
   )
 }
