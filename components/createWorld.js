a6ca00d75c5afc1aeff79ab51ff1110db1ec1e61
Tay <taylor.mleigh@gmail.com>
2020-12-28 21:46:36 -0600

    troubleshooting data querying -- problem either in fetcher function or in useSWR function

--- components/createWorld.js
+++ components/createWorld.js
@@ -9,32 +9,56 @@ const fetcher = async (query) => {
     method: 'POST',
     headers: await generateAuthHeader(),
     body: JSON.stringify({ query }),
-  }).then((res) => res.json()).catch((err) => console.error('jinkies! ', err))
+  }).then((res) => {
+    return res.json();
+  }).catch((err) => console.error('jinkies! ', err))
 }
-
 
 
 
 export default function NewWorld({newRegion, inputs, handleInputChange, handleSubmit}) {
 
-  const {data} = useSWR(GET_ALL_QUESTIONS, fetcher)
+  const {data, error} = useSWR(GET_ALL_QUESTIONS, fetcher)
+  console.log(data)
 
-  if (data && data.error) {
-    console.error(data.error)
-    return <>Error!</>;
+  if (error) {
+    console.error(error)
+    return <div id="F1">Error!</div>;
   }
 
   const questions = data ? data.data.questions : null;
-  console.log(questions);
+  console.log("questions --> ", questions);
 
   const addWorld = (event) => {
     alert('added!');
     return;
   }
 
-  return (
-    <div id="F1">
-      Map through questions
+  if (questions) {
+    return (
+      <div id="F1">
+      World Questions:
+      {
+        data.data.questions.map((section) => {
+
+          return <h2>section.sectionName</h2> + 
+            data.data.questions.questions.map((question) => {
+              return <div key={question.question}>
+                  <h3>{question.question}</h3>
+                  <br/><input placeholder={question.placeholder}></input>
+                </div>
+            })
+        
+        })
+
+      }
     </div>
-  );
+    )
+  } else {
+    return (
+      <div id="F1">
+        no data :(
+      </div>
+    );
+  }
 }
