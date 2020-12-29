b50b78c8ac58e79916a1cae13911e95cc746f322
Tay <taylor.mleigh@gmail.com>
2020-12-28 20:53:03 -0600

    credentials

--- components/RealmClient.js
+++ components/RealmClient.js
@@ -1,7 +1,7 @@
-import { Credentials, App } from 'realm-web';
+import { Credentials, App } from 'realm-web'
 
-const APP_ID = process.env.REALM_APP_ID
-export const REALM_GRAPHQL_ENDPOINT = `https://realm.mongodb.com/api/client/v2.0/app/${APP_ID}/graphql`;
+const APP_ID = process.env.NEXT_PUBLIC_REALM_APP_ID
+export const REALM_GRAPHQL_ENDPOINT = `https://realm.mongodb.com/api/client/v2.0/app/${APP_ID}/graphql`
 
 const app = new App({
   id: APP_ID,
@@ -9,15 +9,15 @@ const app = new App({
 })
 
 export const generateAuthHeader = async () => {
-
   if (!app.currentUser) {
-    await app.logIn(Credentials.anonymous());
+    // If no user is logged in, log in an anonymous user
+    await app.logIn(Credentials.anonymous())
   }
+  // Get a valid access token for the current user
+  const { accessToken } = app.currentUser
 
-  const { accessToken } = app.currentUser;
-
+  // Set the Authorization header, preserving any other headers
   return {
     Authorization: `Bearer ${accessToken}`,
   }
-
 }
